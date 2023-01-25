4a977c2cc7e817e80e77f1741cf712ddef65783d
Cindy Fang <cindy.fang@torontomu.ca>
2023-01-25 15:21:24 -0500

    initial commit

--- face_detector/face_detect.py
+++ face_detector/face_detect.py
@@ -1,8 +1,6 @@
 import cv2
 import sys
 import os
-import pyautogui
-
 
 # Get user supplied values
 imagePath = sys.argv[1]
@@ -24,18 +22,40 @@ faces = faceCascade.detectMultiScale(
     flags = cv2.CASCADE_SCALE_IMAGE
 )
 
+# Create a dictionary for saving every rectangle coordinates
+rect_arr = {"x": [], "y": [], "w": [], "h": []}
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 300, 0), 2)
-    im = pyautogui.screenshot('test.png',region=(100,100,500,400))
+    #print("x",x, "y", y, "w", w, "h",h)
+    rect_arr["x"].append(x)
+    rect_arr["y"].append(y)
+    rect_arr["w"].append(w)
+    rect_arr["h"].append(h)
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 300, 0), 1)
 
+# Show found faces
 cv2.imshow("Faces found", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
 print("Found {0} faces!".format(len(faces)))
 
-os.system("python face_search.py " + imagePath) 
-# import PIL.ImageGrab
-# screenshot = PIL.ImageGrab.grab()
-# screenshot.show()
+# Create cropped images of every found faces and save to directory
+# Set args variable to save image path to every cropped image/face
+args = ""
+for i in range(len(faces)):
+    x1 = (rect_arr["x"][i])
+    y1 = (rect_arr["y"][i])
+    x2 = (rect_arr["x"][i]) + (rect_arr["w"][i])
+    y2= (rect_arr["y"][i]) + (rect_arr["h"][i])
+    crop_image = image[y1:y2, x1:x2]
+    cv2.imshow("cropped" + str(i), crop_image)
+    cv2.imwrite("cropped"+str(i)+".jpg", crop_image)
+    args += "/Users/as/OneDrive/Documents/project/face_detector/cropped"+str(i)+".jpg "
+    cv2.waitKey(0)
+
+# Run celeb_recognition file, passing all found faces as arguments to be identified 
+os.chdir("/Users/as/OneDrive/Documents/project/celeb_recognition")
+os.system("python celeb_recognition.py " + args)
+
+# Optional: run face_search file to google image search the original picture 
+#os.system("python face_search.py " + imagePath) 
